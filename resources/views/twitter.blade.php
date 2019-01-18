6a0bf226a2d8586ef846e6a400cecd33c39a381e
nansol <nancysgithub@gmail.com>
2019-01-18 17:14:26 +0100

    updated

--- resources/views/twitter.blade.php
+++ resources/views/twitter.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
     <link rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css">
     <title>Twitter API</title>
 </head>
@@ -20,20 +21,57 @@
                       <a class="nav-link" href="#">Home</a>
                     </li>
                    </ul>
-                  <form class="form-inline my-2 my-lg-0">
-                    <input class="form-control mr-sm-2" type="text" placeholder="Search">
-                    <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-                  </form>
                 </div>
               </nav>
 
 <div class="container">
+<div class="row " >
+    <div class="col align-self-center m-5 ">
+        <h1 class="text-info">Nancy's Tweets Api</h1>
+    </div>
+</div>
+<div class="row">
+<div class="col align-self-center mb-5">
+        <form class="well" action="{{route('post.tweet')}}" method="POST" enctype="multipart/form-data">
+            @csrf
+            @if(count($errors) > 0)
+            @foreach ($errors->all() as $error)
+            <div class="alert alert-danger"> {{$error}}</div>
+            @endforeach
+            @endif
+            <div class="form-group">
+                <label>Your Tweet</label>
+                <input type="text" name="tweet" class="form-control">
+            </div>
+            <div class="form-group">
+                <label>Upload Filet</label>
+                <input type="file" class="form-control-file" name="images[]" multiple>
+            </div>
+
+            <div class="from-group">
+                <button class="btn btn-success">Create Tweet</button>
+            </div>
+            </form>
+</div>
+
+</div>
 @if(!empty($data))
 @foreach ($data as $key => $tweet )
     <div class="card text-primary bg-white mb-3 align-self-center" style="max-width: 50rem;">
         <div class="card-body">
             <h4 class="card-title"></h4>
             <p class="card-text"><h3>{{$tweet->text}}</h3></p>
+            @if(!empty($tweet->extented_entities->media)){
+            @foreach ($tweet->extented_entities->media as $i)
+            <img src="{{$i->media_url_https}}" style="width:100px;">
+
+            @endforeach
+            }
+            @endif
+            <div class="card-header">
+                    <i class="fas fa-heart"> {{$tweet->favorite_count}}</i>
+                    <i class="fas fa-redo"> {{$tweet->retweet_count}}</i>
+            </div>
         </div>
     </div>
 
